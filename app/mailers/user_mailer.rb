4b5abce41c0af962b487c97251ef4746857a53e0
Isaac Orme <isorme1@gmail.com>
2018-08-13 15:44:54 -0700

    Fixes user mailer class to allow for multiple emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,28 +1,15 @@
-require 'digest/sha2'
-
 class UserMailer < ApplicationMailer
-  default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@robins-blogs.herokuapp.com"
+  default to: -> { User.where(subscription: true).pluck(:email) },
+          from: 'robins-blog.herokuapp.com'
 
   def post_notification(post)
     @post = post
-    @users = User.all
-
-    @users.each do |user|
-      if user.subscription?
-        mail to: "#{user.email}", subject: "Robin has created a new blog post"
-      end
-    end
+    mail(subject: "Robin has created a new blog post")
   end
 
   def comment_notification(post, comment)
     @post = post
     @comment = comment
-    @users = User.all
-
-    @users.each do |user|
-      if user.subscription?
-        mail to: "#{user.email}", subject: "#{@comment.user.email} commented on Robin's blog post #{@post.title}"
-      end
-    end
+    mail(subject: "#{@comment.user.email} commented on Robin's blog post #{@post.title}")
   end
 end
