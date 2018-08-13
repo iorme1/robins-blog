25ac28f171e2f32ca9bc1ab81f08cc0e11d51b7b
Isaac Orme <isorme1@gmail.com>
2018-08-13 14:42:42 -0700

    raise email delivery errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "robins-blog_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { host: 'https://robins-blogs.herokuapp.com' }
 
