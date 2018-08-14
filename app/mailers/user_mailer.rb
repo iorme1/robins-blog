24f87afbd7f8f1d56678acc64ed0037ff8d7a945
Isaac Orme <isorme1@gmail.com>
2018-08-13 17:27:17 -0700

    Fixes duplication of collection in ajax comment creation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,13 @@ class UserMailer < ApplicationMailer
           from: 'robins-blog.herokuapp.com'
 
   def post_notification(post)
+    return if User.where(subscription: true).pluck(:email).empty?
     @post = post
     mail(subject: "Robin has created a new blog post")
   end
 
   def comment_notification(post, comment)
+    return if User.where(subscription: true).pluck(:email).empty?
     @post = post
     @comment = comment
     mail(subject: "#{@comment.user.email} commented on Robin's blog post #{@post.title}")
